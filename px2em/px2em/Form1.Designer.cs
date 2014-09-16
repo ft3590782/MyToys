6267ec1f49ce9391d7d0f52eef3084e43bdad3a2
ft3590782 <jay.xinyun@gmail.com>
2014-09-16 19:13:31 +0800

    new px2em

---
+++ px2em/px2em/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace px2em
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBodyEM = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtPX = new System.Windows.Forms.TextBox();
+            this.txtResultEM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPXOld = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPXNew = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "锁定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBodyEM
+            // 
+            this.txtBodyEM.Location = new System.Drawing.Point(71, 25);
+            this.txtBodyEM.Name = "txtBodyEM";
+            this.txtBodyEM.Size = new System.Drawing.Size(121, 21);
+            this.txtBodyEM.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "计算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPX
+            // 
+            this.txtPX.Location = new System.Drawing.Point(83, 90);
+            this.txtPX.Name = "txtPX";
+            this.txtPX.Size = new System.Drawing.Size(121, 21);
+            this.txtPX.TabIndex = 3;
+            // 
+            // txtResultEM
+            // 
+            this.txtResultEM.Location = new System.Drawing.Point(82, 171);
+            this.txtResultEM.Name = "txtResultEM";
+            this.txtResultEM.ReadOnly = true;
+            this.txtResultEM.Size = new System.Drawing.Size(121, 21);
+            this.txtResultEM.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "结果EM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "待转换像素";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BodyEM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "原始画布像素";
+            // 
+            // txtPXOld
+            // 
+            this.txtPXOld.Location = new System.Drawing.Point(97, 52);
+            this.txtPXOld.Name = "txtPXOld";
+            this.txtPXOld.Size = new System.Drawing.Size(43, 21);
+            this.txtPXOld.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "比例";
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Location = new System.Drawing.Point(83, 144);
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.ReadOnly = true;
+            this.txtPercent.Size = new System.Drawing.Size(121, 21);
+            this.txtPercent.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(181, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "目标画布像素";
+            // 
+            // txtPXNew
+            // 
+            this.txtPXNew.Location = new System.Drawing.Point(264, 52);
+            this.txtPXNew.Name = "txtPXNew";
+            this.txtPXNew.Size = new System.Drawing.Size(43, 21);
+            this.txtPXNew.TabIndex = 13;
+            this.txtPXNew.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 262);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPXNew);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPXOld);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultEM);
+            this.Controls.Add(this.txtPX);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBodyEM);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBodyEM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtPX;
+        private System.Windows.Forms.TextBox txtResultEM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPXOld;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPercent;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPXNew;
+    }
+}
+
---
+++ px2em/px2em/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace px2em
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            doTrans();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtBodyEM.ReadOnly)
+            {
+                txtBodyEM.ReadOnly = false;
+                txtPXNew.ReadOnly = false;
+                txtPXOld.ReadOnly = false;
+            }
+            else
+            {
+                txtBodyEM.ReadOnly = true;
+                txtPXNew.ReadOnly = true;
+                txtPXOld.ReadOnly = true;
+            }
+        }
+
+        private void doTrans()
+        {
+
+            float bodyem = getContext(txtBodyEM.Text.Trim(), 0);
+            float px = getContext(txtPX.Text.Trim(), 0);
+            float resultem = 0;
+            resultem = bodyem / 16 * px;
+            txtResultEM.Text = Math.Round(resultem, 2).ToString();
+
+            float oldpx = getContext(txtPXOld.Text.Trim(), 0);
+            float newpx = getContext(txtPXNew.Text.Trim(), 0);
+            float resultpercent = 0;
+            resultpercent = px / oldpx * 100;
+            txtPercent.Text = Math.Round(resultpercent, 2).ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private float getContext(string obj, float defaultresult)
+        {
+            float result = 0;
+            if (IsFloat(obj))
+            {
+                result = float.Parse(obj.Trim());
+            }
+            else
+                result = defaultresult;
+            return result;
+        }
+
+        //判断字符串是否为浮点数
+        public static bool IsFloat(string str)
+        {
+            string regextext = @"^(-?\d+)(\.\d+)?$";
+            Regex regex = new Regex(regextext, RegexOptions.None);
+            return regex.IsMatch(str.Trim());
+        }
+    }
+}
